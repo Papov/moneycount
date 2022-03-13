16ddbf660f4da240fb64c3c320376c064780d53f
Vladyslav Popov <vladyslav.popov@onsolve.com>
2022-03-13 21:57:20 +0200

    just get started. Getting balance and list of transactions. Dump functionality.

--- App.js
+++ App.js
@@ -1,112 +1,107 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button, ActivityIndicator} from 'react-native';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import moment from 'moment/moment';
+import CryptoJS from 'crypto-js';
+import axios from 'axios';
+import Xml2js from 'react-native-xml2js';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loaderContainer: {
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const DATE_FORMAT = 'DD.MM.YYYY';
+
+const DATE_RANGE = {
+  today: () => moment().format(DATE_FORMAT),
+  monthAgo: () => moment().subtract(1, 'month').format(DATE_FORMAT),
+};
+
+const makeSignature = (data, password) => {
+  const hash = CryptoJS.MD5(`${data}${password}`);
+  const signature = CryptoJS.SHA1(hash.toString(CryptoJS.enc.Hex));
+  return signature.toString(CryptoJS.enc.Hex);
+};
+
+const doRequest = (setAmount, setLoading) => async () => {
+  setLoading(true);
+  const psw = '';
+  const card = '';
+  const id = '';
+
+  const balance = `<oper>cmt</oper>
+            <wait>90</wait>
+            <test>0</test>
+            <payment id="">
+                <prop name="cardnum" value="${card}" />
+                <prop name="country" value="UA" />
+            </payment>`;
+
+  const list = `<oper>cmt</oper>
+            <wait>90</wait>
+            <test>0</test>
+            <payment id="">
+                <prop name="sd" value="${DATE_RANGE.monthAgo()}"/>
+                <prop name="ed" value="${DATE_RANGE.today()}"/>
+                <prop name="cardnum" value="${card}"/>
+                <prop name="country" value="UA" />
+            </payment>`;
+
+  const signature = makeSignature(balance, psw);
+
+  const request = `<?xml version="1.0" encoding="UTF-8"?>
+            <request version="1.0">
+                <merchant>
+                    <id>${id}</id>
+                    <signature>${signature}</signature>
+                </merchant>
+                <data>
+                    ${balance}
+                </data>
+            </request>`;
+
+  const response = await axios.post(
+    'https://api.privatbank.ua/p24api/balance',
+    request,
+    {headers: {'Content-Type': 'text/xml'}},
+  );
+  Xml2js.parseString(response.data, {trim: true}, (err, res) => {
+    setAmount(`${res.response.data[0].info[0].cardbalance[0].balance} UAH`);
+    setLoading(false);
+  });
+};
+
+const App = () => {
+  const [amount, setAmount] = useState('0 UAH');
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={s.container}>
+      <Text>Текущий счет:</Text>
+      <Text>{amount}</Text>
+      <Button
+        title="Синхронизировать"
+        onPress={doRequest(setAmount, setIsLoading)}
+      />
+      {isLoading && (
+        <View style={s.loaderContainer}>
+          <ActivityIndicator size="large" />
+        </View>
+      )}
     </View>
   );
 };
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
